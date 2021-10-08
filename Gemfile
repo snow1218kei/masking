b0fe73245cb7db88c20e9ffaa811a9b012f3b405
EC2 Default User <ec2-user@ip-10-0-20-195.ap-northeast-1.compute.internal>
2021-10-08 05:00:29 +0000

    .gitignore is now working

--- Gemfile
+++
@@ -1,6 +0,0 @@
-source "https://rubygems.org"
-
-gem "activerecord"
-gem "activerecord-import"
-gem "dotenv"
-gem "mysql2"
--- user.rb
+++
@@ -1,7 +0,0 @@
-require "./active_record"
-
-class User < ActiveRecord::Base
-  self.table_name = "users"
-  validates_presence_of :username
-  validates_presence_of :email
-end
